ec43c97a5187e372993089c6fb3a4714f11e558d
Edouard COMTET <edouard.comtet@gmail.com>
2021-12-13 09:59:34 +0100

    🎉 complete day12 challenge 1

---
+++ day12/challenge1/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
+const FILE_LOCATION = "./input.txt"
+
+type Path struct {
+	caveA string
+	caveB string
+}
+
+func main() {
+	world := readFile()
+	toExplore := [][]string{{"start"}}
+	explored := true
+
+	for explored {
+		explored = false
+
+		for i := len(toExplore) - 1; i >= 0; i-- {
+			pathToExplore := toExplore[i]
+			fmt.Printf("Exploring %d %v\n", i, pathToExplore)
+			lastElement := pathToExplore[len(pathToExplore)-1]
+
+			if lastElement == "end" {
+				fmt.Println("Already an endpath")
+				continue
+			}
+
+			toExplore = append(toExplore[:i], toExplore[i+1:]...)
+			explored = true
+			endpoints := explore(world, lastElement)
+
+			fmt.Printf("Found new element %v\n", endpoints)
+
+			for _, endpoint := range endpoints {
+				newPath := append([]string{}, pathToExplore...)
+				newPath = append(newPath, endpoint)
+
+				if isValidPath(newPath) {
+					toExplore = append(toExplore, newPath)
+				} else {
+					fmt.Printf("This new path is not valid %v\n", newPath)
+				}
+			}
+		}
+	}
+
+	fmt.Println(len(toExplore))
+}
+
+func readFile() []Path {
+	var world []Path
+
+	file, err := os.Open(FILE_LOCATION)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		points := strings.Split(text, "-")
+
+		path := Path{
+			caveA: points[0],
+			caveB: points[1],
+		}
+
+		world = append(world, path)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return world
+}
+
+func explore(world []Path, cave string) []string {
+	var nextPoint []string
+
+	for _, path := range world {
+		if path.caveA == cave {
+			nextPoint = append(nextPoint, path.caveB)
+		}
+
+		if path.caveB == cave {
+			nextPoint = append(nextPoint, path.caveA)
+		}
+	}
+
+	return nextPoint
+}
+
+func isValidPath(path []string) bool {
+	var onlyOnePassCaveAllowed []string
+
+	for _, cave := range path {
+		if unicode.IsLower([]rune(cave)[0]) {
+			onlyOnePassCaveAllowed = append(onlyOnePassCaveAllowed, cave)
+		}
+	}
+
+	for posA, caveA := range onlyOnePassCaveAllowed {
+		for posB, caveB := range onlyOnePassCaveAllowed {
+			if posA != posB && caveA == caveB {
+				return false
+			}
+		}
+	}
+
+	return true
+}
