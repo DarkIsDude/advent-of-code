4339c7c56f4690c192844b63d4f25a4a58ab278c
DarkIsDude <edouard.comtet@gmail.com>
2023-12-05 11:21:16 +0100

    ✨ resolve day4 challenge 2

--- 2023/day4/challenge2/main.go
+++ 2023/day4/challenge2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -19,41 +20,49 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	sum := 0
+	numberOfCards := map[int]int{}
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		split := strings.Split(line, ": ")
-		card := strings.Replace(split[0], "Card ", "", -1)
+		cardAsString := strings.Replace(split[0], "Card ", "", -1)
+		card, err := strconv.Atoi(strings.TrimSpace(cardAsString))
 
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		numberOfCards[card] = numberOfCards[card] + 1
 		lotery := strings.Split(strings.TrimSpace((split[1])), " | ")
 
 		winning := strings.Split(lotery[0], " ")
 		numbers := strings.Split(lotery[1], " ")
 
-		points := 0
+		numberOfWinning := 0
 
 		for _, number := range numbers {
 			for _, win := range winning {
 				if number == win && number != "" && win != "" {
-					if points == 0 {
-						points = 1
-					} else {
-						points *= 2
-					}
+					numberOfWinning++
 				}
 			}
 		}
 
-		sum += points
-
-		fmt.Printf("For card %s, the points are %d\n", card, points)
+		for i := 1; i <= numberOfWinning; i++ {
+			numberOfCards[card + i] += numberOfCards[card]
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("The total points are %d\n", sum)
+	sum := 0
+	for key, value := range numberOfCards {
+		sum += value
+		fmt.Printf("Card %d: %d\n", key, value)
+	}
+
+	fmt.Printf("Total: %d\n", sum)
 }
